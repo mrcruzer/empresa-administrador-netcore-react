c17d5cd85653a3bae534d1354d8bf90e802ea9c9
Isaac Avelino <mrcruzer>
2020-04-27 18:02:52 -0400

    up

---
+++ empresa-administrador-api/Models/Empleado.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace empresa_administrador_api.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Apellido { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Direccion { get; set; }
+
+        public int Edad { get; set; }
+
+
+        public int Nomina { get; set; }
+
+        [Column(TypeName = "VARCHAR(50)")]
+        public string TipoNomina { get; set; }
+
+
+        public int Telefono { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Posicion { get; set; }
+    }
+}
--- empresa-administrador-api/Startup.cs
+++ empresa-administrador-api/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using empresa_administrador_api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,15 @@ namespace empresa_administrador_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //object p = services.AddRouteAnalyzer();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.WithOrigins("http://localhost:4200", "http://localhost:3000", "https://localhost:44317", "http://localhost:3000/", "http://mywebsite.com")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +46,8 @@ namespace empresa_administrador_api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
