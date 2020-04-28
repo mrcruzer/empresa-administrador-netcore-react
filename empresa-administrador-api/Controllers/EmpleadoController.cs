38541698340186b4fbdf9360fb483e20ca9da039
Isaac Avelino <mrcruzer>
2020-04-28 00:21:37 -0400

    up

---
+++ empresa-administrador-api/Controllers/EmpleadoController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using empresa_administrador_api.Models;
+
+namespace empresa_administrador_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmpleadoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Empleado
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Empleado>>> GetEmpleados()
+        {
+            return await _context.Empleados.ToListAsync();
+        }
+
+        // GET: api/Empleado/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empleado>> GetEmpleado(int id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return empleado;
+        }
+
+        // PUT: api/Empleado/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmpleado(int id, Empleado empleado)
+        {
+            if (id != empleado.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(empleado).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpleadoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Empleado
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Empleado>> PostEmpleado(Empleado empleado)
+        {
+            _context.Empleados.Add(empleado);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmpleado", new { id = empleado.Id }, empleado);
+        }
+
+        // DELETE: api/Empleado/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Empleado>> DeleteEmpleado(int id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            _context.Empleados.Remove(empleado);
+            await _context.SaveChangesAsync();
+
+            return empleado;
+        }
+
+        private bool EmpleadoExists(int id)
+        {
+            return _context.Empleados.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ empresa-administrador-api/Migrations/20200427221432_initia.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace empresa_administrador_api.Migrations
+{
+    public partial class initia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    Apellido = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    Direccion = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    Edad = table.Column<int>(nullable: false),
+                    Nomina = table.Column<int>(nullable: false),
+                    TipoNomina = table.Column<string>(type: "VARCHAR(50)", nullable: true),
+                    Telefono = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    Posicion = table.Column<string>(type: "VARCHAR(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empleados");
+        }
+    }
+}
