5ec92382a6e8eb56ef481a7c5a7c659a3f389044
Isaac Avelino <isaacavelino14@gmail.com>
2022-08-31 00:45:35 -0400

    add models skills and education

---
+++ empresa-administrador-api/Models/Nationality.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace empresa_administrador_api.Models
+{
+    public class Nationality
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "VARCHAR(50)")]
+        public string Name { get; set; }
+
+        
+    }
+}
