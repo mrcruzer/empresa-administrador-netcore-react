1425b12104958baf731c1fd9ea1d9ad08870e065
Isaac Avelino <isaacavelino14@gmail.com>
2022-08-31 00:47:05 -0400

    add other models

---
+++ empresa-administrador-api/Models/Language.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace empresa_administrador_api.Models
+{
+    public class Language
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "VARCHAR(50)")]
+        public string Name { get; set; }
+
+
+        
+    }
+}
---
+++ empresa-administrador-api/Models/License.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace empresa_administrador_api.Models
+{
+    public class License
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "VARCHAR(50)")]
+        public string Name { get; set; }
+
+
+
+
+    }
+}
