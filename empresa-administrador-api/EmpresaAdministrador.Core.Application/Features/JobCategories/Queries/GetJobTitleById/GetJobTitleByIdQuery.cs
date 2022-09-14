b4c85641c658376aadd48fb0cbce94e4836f05ba
Isaac Avelino <isaacavelino14@gmail.com>
2022-09-13 20:00:17 -0400

    add controllers job titles and job category

---
+++ empresa-administrador-api/EmpresaAdministrador.Core.Application/Features/JobCategories/Queries/GetJobTitleById/GetJobTitleByIdQuery.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using EmpresaAdministrador.Core.Application.Dtos.API.JobTitle;
+using EmpresaAdministrador.Core.Application.Interfaces.Repositories;
+using MediatR;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmpresaAdministrador.Core.Application.Features.JobCategories.Queries.GetJobTitleById
+{
+    public class GetJobTitleByIdQuery : IRequest<JobTitleResponse>
+    {
+        [SwaggerParameter(Description = "Id de la nacionalidad")]
+        public int Id { get; set; }
+
+        public class GetJobTitleByIdQueryHandler : IRequestHandler<GetJobTitleByIdQuery, JobTitleResponse>
+        {
+            public readonly IJobTitleRepository _jobTitleRepo;
+            public readonly IMapper _mapper;
+            public GetJobTitleByIdQueryHandler(IJobTitleRepository jobTitleRepo, IMapper mapper )
+            {
+                _jobTitleRepo = jobTitleRepo;
+                _mapper = mapper;
+            }
+
+            public async Task<JobTitleResponse> Handle(GetJobTitleByIdQuery request, CancellationToken cancellationToken)
+            {
+                var jobTitle = await _jobTitleRepo.GetByIdAsync(request.Id);
+                return _mapper.Map<JobTitleResponse>(jobTitle);
+            }
+        }
+    }
+}
---
+++ empresa-administrador-api/EmpresaAdministrador.Core.Application/Features/JobCategories/Queries/ListJobTitles/ListJobTitleQuery.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using EmpresaAdministrador.Core.Application.Dtos.API.JobTitle;
+using EmpresaAdministrador.Core.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmpresaAdministrador.Core.Application.Features.JobCategories.Queries.ListJobTitles
+{
+    public class ListJobTitleQuery : IRequest<IList<JobTitleResponse>>
+    {
+        public class ListJobTitleQueryHandler : IRequestHandler<ListJobTitleQuery, IList<JobTitleResponse>>
+        {
+            private readonly IJobTitleRepository _jobTitleRepo;
+            private readonly IMapper _mapper;
+
+            public ListJobTitleQueryHandler(IJobTitleRepository jobTitleRepo, IMapper mapper)
+            {
+                _jobTitleRepo = jobTitleRepo;
+                _mapper = mapper;
+            }
+
+            public async Task<IList<JobTitleResponse>> Handle(ListJobTitleQuery request, CancellationToken cancellationToken)
+            {
+                var jobTitle = await _jobTitleRepo.GetAllAsync();
+                return _mapper.Map<List<JobTitleResponse>>(jobTitle);
+            }
+        }
+    }
+}
